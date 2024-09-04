0294551a98a68da32ff20edff7e2be8d457dfd5d
Umut Yenidil <umut.yenidil9@gmail.com>
2024-09-04 11:07:35 +0300

    Initial Commit

---
+++ SwiftUI-Instagram/App/SwiftUI_InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_InstagramApp.swift
+//  SwiftUI-Instagram
+//
+//  Created by Umut Yenidil on 4.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-Instagram/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Instagram
+//
+//  Created by Umut Yenidil on 4.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
